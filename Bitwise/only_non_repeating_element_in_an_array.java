15c628106440fdf27866781b80c2b0f1a856053c
Nishant Jaiswal <72241228+jaiswalnj@users.noreply.github.com>
2023-04-12 00:16:51 +0530

    initial commit

--- Bitwise/only_non_repeating_element_in_an_array.java
+++ Bitwise/only_non_repeating_element_in_an_array.java
@@ -1,9 +1,20 @@
 package Bitwise;
-
 import java.util.Scanner;
 
 public class only_non_repeating_element_in_an_array {
-    Scanner sc = new Scanner(Syatem.in);
-    int arr[]= new int{};
-
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter size of array");
+    int n=sc.nextInt();
+    int res=0;
+    int arr[]= new int[n];
+    System.out.println("Enter elements");
+    for(int i=0;i<arr.length;i++){
+        arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<arr.length;i++){
+        res=arr[i]^res;
+    }
+    System.out.println("only non repeating element in an array:"+res);
+}
 }
