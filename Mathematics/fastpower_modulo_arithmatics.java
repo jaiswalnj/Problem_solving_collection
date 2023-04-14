7f3b32acd6e9e249d4d845fbd60a922cc4546fc3
Nishant Jaiswal <72241228+jaiswalnj@users.noreply.github.com>
2023-04-15 00:02:00 +0530

    initial commit

--- Mathematics/fastpower_modulo_arithmatics.java
+++ Mathematics/fastpower_modulo_arithmatics.java
@@ -3,11 +3,11 @@ package Mathematics;
 import java.util.Scanner;
 
 class  moduloarithmatics{
-    public static long fastprime(long a,long b,int n){
+    public static long fastpower(long a,long b,int n){
         long res=1;
         while (b>0){
             if((b&1)!=0){
-                res=(res*a%n)%n;
+                res=(res * a%n)%n;
             }
             a=(a%n * a%n)%n;
             b=b>>1;
@@ -16,12 +16,13 @@ class  moduloarithmatics{
     }
     public static void main(String[] args){
         Scanner inp=new Scanner(System.in);
-        System.out.println("Please provide the range");
+        System.out.println("Please provide the number");
         long a=inp.nextLong();
+        System.out.println("Please provide the power");
         long b=inp.nextLong();
         System.out.println("Please provide value of n");
         int n=inp.nextInt();
-        long tp=fastprime(a, b, n);
+        long tp=fastpower(a, b, n);
         System.out.println("total no. of prime in given range ="+tp+"x"+n);
     }
 }
