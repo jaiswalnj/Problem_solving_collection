ff7230febc6772e9f6b94b83040966f03c739d1e
Nishant Jaiswal <72241228+jaiswalnj@users.noreply.github.com>
2023-05-23 10:59:58 +0530

    initial commit

---
+++ HashMap/kth_largest_element_in_a_stream.java
@@ -0,0 +1,30 @@
+package HashMap;
+
+import java.util.*;
+
+class KthLargest {
+    private static int k;
+    private PriorityQueue<Integer> heap;
+    
+    public KthLargest(int k, int[] nums) {
+        KthLargest.k = k;
+        heap = new PriorityQueue<>();
+        
+        for (int num: nums) {
+            heap.offer(num);
+        }
+        
+        while (heap.size() > k) {
+            heap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        heap.offer(val);
+        if (heap.size() > k) {
+            heap.poll();
+        }
+
+        return heap.peek();
+    }
+}
