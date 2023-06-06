487f3766fdddf423dd029e284b6e9210affe37e0
Nishant Jaiswal <72241228+jaiswalnj@users.noreply.github.com>
2023-06-06 21:42:36 +0530

    initial commit

---
+++ Mathematics/can_you_make_ an_AP.java
@@ -0,0 +1,36 @@
+package Mathematics;
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        int minValue = Arrays.stream(arr).min().getAsInt();
+        int maxValue = Arrays.stream(arr).max().getAsInt();
+        int n = arr.length;
+        if ((maxValue - minValue) % (n - 1) != 0) {
+            return false;
+        }
+        
+        int diff = (maxValue - minValue) / (n - 1);
+        int i = 0;
+        
+        while (i < n) {
+            if (arr[i] == minValue + i * diff) {
+                i += 1;
+            } else if ((arr[i] - minValue) % diff != 0) {
+                return false;
+                
+            } else {
+                int j = (arr[i] - minValue) / diff;
+                if (arr[i] == arr[j]) {
+                    return false;
+                }
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+        return true;
+    }
+}
