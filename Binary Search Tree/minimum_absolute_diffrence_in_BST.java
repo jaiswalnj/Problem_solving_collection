de18453cdcfe8d55bc8bdcdce05d5a4ebe4ca550
Nishant Jaiswal <72241228+jaiswalnj@users.noreply.github.com>
2023-06-14 08:05:14 +0530

    initial commit

---
+++ Binary Search Tree/minimum_absolute_diffrence_in_BST.java
@@ -0,0 +1,19 @@
+class Solution {
+    int minDifference = Integer.MAX_VALUE;
+    TreeNode prevNode;
+
+    void inorderTraversal(TreeNode node) {
+        if (node == null) return;
+
+        inorderTraversal(node.left);
+        if (prevNode != null) minDifference = Math.min(minDifference, node.val - prevNode.val);
+
+        prevNode = node;
+        inorderTraversal(node.right);
+    }
+
+    int getMinimumDifference(TreeNode root) {
+        inorderTraversal(root);
+        return minDifference;
+    }
+};
